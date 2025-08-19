cf79a3841c07a2a43cbdcf7f8487a8d5152ad605
EduardoHuarcaya-DEV <waltereduardohm@gmail.com>
2025-08-19 10:10:50 -0500

    Añadiendo prefijo global y configuracion para validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api/v1'); // Prefijo global para todas las rutas
+  // Ejemplo: /api/v1/users
+
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true, // Elimina propiedades no definidas en el DTO
+    forbidNonWhitelisted: true, // Lanza error si hay propiedades no definidas
+    transform: true, // Transforma automáticamente los datos
+  }));
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
